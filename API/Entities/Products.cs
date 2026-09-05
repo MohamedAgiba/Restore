85a77a376a3d28fa3c78faec1032fe30bd6f7c82
MohamedAgiba <mohamed.agiba@cic.ae>
2026-09-05 20:51:39 +0300

    Fix project file paths and update assembly information; add Products entity class

---
+++ API/Entities/Products.cs
@@ -0,0 +1,16 @@
+﻿namespace API;
+//each class I create is going to have a namespace where it logically resides,
+//whick prevent from having conflicts with classes that got same name. 
+
+public class Products
+{
+    public int Id { get; set; }
+    public required string Name { get; set; } 
+    // = "" || ? || required modifier 
+    public required string Description { get; set; }
+    public long Price { get; set; }
+    public required string PictureUrl { get; set; }
+    public required string Type { get; set; }
+    public required string Brand { get; set; }
+    public int QuantityInStock { get; set; }
+}
